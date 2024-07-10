bc57a16b71a997415721fc173bed751e0a85688f
mBartoldus <mbartoldus@gmail.com>
2024-07-10 00:03:09 -0400

    added return types

--- src/messaging/control_queue.ts
+++ src/messaging/control_queue.ts
@@ -1,7 +1,7 @@
 const _controlQueue = Symbol('control message queue')
 
 // deno-lint-ignore no-explicit-any
-export function createControlQueue(context: any) {
+export function createControlQueue(context: any): void {
     Object.defineProperty(context, _controlQueue, {
         value: [],
         enumerable: false
@@ -9,13 +9,13 @@ export function createControlQueue(context: any) {
 }
 
 // deno-lint-ignore no-explicit-any
-export function sendControlMessage(context: any, msg: any) {
+export function sendControlMessage(context: any, msg: any): void {
     //@ts-ignore assume _ctrlQueue will be defined as a non-enumerable property
     context[_controlQueue].push(msg)
 }
 
 // deno-lint-ignore no-explicit-any
-export function getControlQueue(context: any) {
+export function getControlQueue(context: any): any[] {
     //@ts-ignore assume _ctrlQueue will be defined as a non-enumerable property
     return context[_controlQueue]
 }
